f642700da247844d757254f9ac783245f4bac909
herify <herifyram@gmail.com>
2025-03-28 14:57:05 -0400

    upload ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div className = "header">
+            <h1> ????? </h1>
+        </div>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,77 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from 'react';
 
+const BASE = 'http://localhost:8000';
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [ src, setSrc ] = useState(null);
+    const [ img, setImg ] = useState(null);
+    const [ output, setOutput ] = useState(null);
+    const handleImage = (e) => {
+        console.log('image selected :',e.target.files);
+        if (e.target.files.length){
+            const file = e.target.files[0];
+            const new_src = URL.createObjectURL(file)
+            setSrc(new_src)
+            setImg(file)
+        }
+    }
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('submit')
+        const url = `${BASE}/upload`;
+        try{
+            const formData = new FormData();
+            formData.append('image', img);
+            const res = await fetch(url, {
+                method : 'POST',
+                body : formData,
+            });
+            const response = await res.json();
+            console.log('response from server :',response);
+            setOutput(response);
+        }catch(err){
+            console.log('error in handleSubmit :', err);
+        }
+    }
+    return (
+        <div className={styles.page}>
+            <div className ={styles.input}>
+                <div className={styles.control}> 
+                    <label>
+                    select an Image
+                        <input 
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImage}
+                        className={styles.input}
+                        />
+                    </label>
+                </div>
+                <div className={styles.preview}>
+                    <p> preview of image </p>
+                    <img
+                        className={styles.img}
+                        src = {src}
+                        alt = "preview image"
+                    />
+                </div>
+                <button
+                    onClick = {handleSubmit}
+                >
+                    Submit
+                </button>
+            </div>
+            <div id ={styles.output}>
+                <p> output </p>
+                { output &&
+                    <>
+                        <p> {output} </p>
+                    </>
+                }
+            </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
